9481079d981c3ba38499209656b2bce7d839b45a
Anne-Claire Ho Si <anneclaire.hosi@gmail.com>
2025-06-09 11:33:22 +0200

    (ref) Rename Polecards
    ref

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 
 export default {
     expo: {
-      name: "polecombo",
-      slug: "polecombo",
+      name: "polecards",
+      slug: "polecards",
       extra: {
         supabaseUrl: process.env.SUPABASE_URL,
         supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
